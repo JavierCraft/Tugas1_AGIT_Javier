5bd36130a71712592edfeaaed105cf48c7a9d42e
JavierCraft <137738041+JavierCraft@users.noreply.github.com>
2024-06-21 16:26:10 +0700

    Add files via upload

---
+++ tugas1_agit_javier/src/main/java/id/ac/astra/polytechnic/tugas1_agit_javier/Tugas1AgitJavierApplication.java
@@ -0,0 +1,13 @@
+package id.ac.astra.polytechnic.tugas1_agit_javier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1AgitJavierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tugas1AgitJavierApplication.class, args);
+    }
+
+}
---
+++ tugas1_agit_javier/src/test/java/id/ac/astra/polytechnic/tugas1_agit_javier/Tugas1AgitJavierApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.astra.polytechnic.tugas1_agit_javier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1AgitJavierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
